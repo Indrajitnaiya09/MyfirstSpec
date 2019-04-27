6fbcc871fba4a007830aaaeef498eb89d383f529
Indrajitkloc <indrajit.naiya@kloctechnologies.co.in>
2019-04-27 18:20:49 +0530

    refactor: Calculator program code refactored

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,35 +1,24 @@
 require 'pry'
 class Calculator
     def initialize(num1, num2)
-        @num1 = num1
-        @num2 = num2
+        if (num1.class != String && num2.class != String)
+            @num1 = num1
+            @num2 = num2
+            @flag = true
+        else
+            @flag = false
+        end
     end
     def add
-        if (@num1.class != String && @num2.class != String)
-            return(@num1 + @num2)
-        else
-            return false
-        end
+        (@flag == true) ? (@num1 + @num2) : false
     end
     def substract
-        if (@num1.class != String && @num2.class != String)
-            return(@num1 - @num2)
-        else
-            return false
-        end
+        @flag == true ? (@num1 - @num2) : false
     end
     def multiply
-        if (@num1.class != String && @num2.class != String)
-            return(@num1 * @num2)
-        else
-            return false
-        end
+        (@flag == true) ? (@num1 * @num2) : false
     end
     def divide
-        if (@num1.class != String && @num2.class != String && @num2 != 0)
-            return(@num1 / @num2)
-        else
-            return false
-        end
+        ( @num2 != 0) && (@flag == true) ? (@num1 / @num2) : false
     end
 end
